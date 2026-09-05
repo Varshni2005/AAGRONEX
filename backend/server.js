89faf1a5078f494b671baa87789a0eb8967cf429
Varshni R P <varshnirp2005@gmail.com>
2026-09-05 14:05:33 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ mongoose
 
 app.use("/api/query", queryRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
